fca92b9800256f6f6d01f8bce4371c2ad545d3f5
Asim Poptani <asim@poptani.com>
2020-06-05 11:59:37 +0100

    WIP: mHealth_get_dataset

--- readData.py
+++ readData.py
@@ -1,1 +1,17 @@
-# This function readData takes in 
+# Give data in a dictionary : { subjectX : 2D array of data, subjectY : 2D array of data}
+def mHealth_get_dataset():
+    # Root dir
+    dir_to_files = './mlhealth-data/'
+
+    # get our files
+    files=[]
+    # Go through append files
+    for index in range(1,11):
+        files.append(f"mHealth_subject{index}.log")
+
+    # Read data
+    for uri_name in files:
+        uri=dir_to_files+uri_name
+        mlhealth_file=open(uri,'r')
+        print(mlhealth_file.read())
+        mlhealth_file.close()
